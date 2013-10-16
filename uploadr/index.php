755304980ea38eb02b65ce19df172fdc68d83f88
Florian Binder <fb@sideshow-systems.de>
2013-10-16 20:41:22 +0200

    added config file, added uploader file, added js vendor download functionality

---
+++ uploadr/index.php
@@ -0,0 +1,55 @@
+<?php
+$config = require_once '../config.php';
+
+// TODO: put this in own class and add unit tests!
+$jsLibs = array(
+	array(
+		'lib' => 'jquery-1.10.2.min.js',
+		'downloadUrl' => 'http://code.jquery.com/jquery-1.10.2.min.js'
+	),
+	array(
+		'lib' => 'less-1.4.1.min.js',
+		'downloadUrl' => 'https://raw.github.com/less/less.js/master/dist/less-1.4.1.min.js'
+	)
+);
+foreach ($jsLibs as $libData) {
+	$libFile = str_replace('/uploadr', '', dirname(__FILE__)) . '/misc/js/vendor/' . $libData['lib'];
+	if (!file_exists($libFile)) {
+		file_put_contents($libFile, file_get_contents($libData['downloadUrl']));
+	}
+}
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>FileSharer v<?php echo $config['version']; ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="shortcut icon" href="../misc/pics/favicon.ico" />
+		<link rel="apple-touch-icon" href="../misc/pics/icon.png" />
+
+		<link rel="stylesheet/less" type="text/css" href="../misc/less/master.less" />
+		<script src="../misc/js/vendor/less-1.4.1.min.js"></script>
+		<script src="../misc/js/vendor/jquery-1.10.2.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
+
+		<div id="main_container">
+			<div id="upload_wrapper">
+				<h1>FileSharer</h1>
+				<form>
+					content...<br />content...<br />content...<br />content...<br />content...<br />content...<br />
+				</form>
+			</div>
+		</div>
+
+	</body>
+</html>
