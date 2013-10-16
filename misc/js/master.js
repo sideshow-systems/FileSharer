7df3026362166e00f42ca7a8a2941867084ab781
Florian Binder <fb@sideshow-systems.de>
2013-10-16 21:53:48 +0200

    added dropzone event listeners

--- misc/js/master.js
+++ misc/js/master.js
@@ -9,6 +9,18 @@ Dropzone.options.dropzoneForm = {
 	paramName: "file",
 	maxFiles: 1,
 	maxFilesize: 200, // MB
+	init: function() {
+		// TODO: event listener to show generated file url
+		this.on('complete', function(file) {
+			console.log(file);
+			alert("File transfer is complete. Your url is http://tiny.bla.com/?123adsfasdf");
+		});
+
+		// TODO: show progress here...
+		this.on('uploadprogress', function(file, progress, bytesSent) {
+			console.log(arguments);
+		});
+	},
 	accept: function(file, done) {
 		if (file.name === "justinbieber.jpg") {
 			done("Naha, you don't.");
