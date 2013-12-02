832ede9fdafa35b71d2fb54e500bf4449ff253a5
Florian Binder <florian.binder@mp-muenchen.de>
2013-12-02 14:16:24 +0100

    added host to url

--- ul.php
+++ ul.php
@@ -8,18 +8,18 @@ $storeFolder = 'data';   //2
 
 if (!empty($_FILES)) {
 	$keys = array();
-	
+
 	$helper = new de\sideshowsystems\filesharer\FileSharerHelper($config);
-	
-	foreach($_FILES as $file) {
+
+	foreach ($_FILES as $file) {
 		$tempFile = $file['tmp_name']; //3
 		$realName = $file['name'];
 		$mimeType = $file['type'];
 
 		$key = $helper->consumeUpload($tempFile, $realName, $mimeType);
-		
+
 		$keys[$realName] = $key;
-		echo(dirname($_SERVER['PHP_SELF']) . '/?' . $key . "\n");
+		echo('http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/?' . $key . "\n");
 	}
 
 	//print_r($keys);//"http://fs.example.com/?1j1239198jkjkjlsid212";
