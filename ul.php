a4b49303d8a84c05381e9e86e36b198a9fdcf273
Florian Binder <florian.binder@mp-muenchen.de>
2015-06-10 13:23:20 +0200

    hide error log statements

--- ul.php
+++ ul.php
@@ -7,21 +7,15 @@ if (!empty($_FILES)) {
 
 	$helper = new de\sideshowsystems\filesharer\FileSharerHelper($config);
 	
-	error_log(print_r($_FILES, true));
-	
 	$multiple = false;
 	
 	if (!empty($_FILES) && !empty($_FILES['file']) && !empty($_FILES['file']['name']) && count($_FILES['file']['name']) > 1) {
 		$multiple = true;
 	}
 
-	error_log(print_r($multiple, true));
-	
 	// Single file
 	if (!$multiple) {
-		error_log('SINGLE');
 		foreach ($_FILES as $file) {
-			error_log(print_r($file, true));
 			$tempFile = $file['tmp_name'][0]; //3
 			$realName = $file['name'][0];
 			$mimeType = $file['type'][0];
